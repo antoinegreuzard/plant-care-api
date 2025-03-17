ce1ff68912ab3b5b32c083306490c4c4a62dea1f
Antoine GREUZARD <antoin.greuzard@gmail.com>
2025-03-17 13:37:11 +0100

    feat: add calendrier into Plant model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import timedelta, date
 
 
 class Plant(models.Model):
@@ -32,6 +33,25 @@ class Plant(models.Model):
     created_at = models.DateTimeField(
         auto_now_add=True, verbose_name="Date d’ajout")
 
+    # Ajout des fréquences d'entretien
+    watering_frequency = models.IntegerField(
+        default=7, verbose_name="Fréquence d’arrosage (jours)")
+    fertilizing_frequency = models.IntegerField(
+        default=30, verbose_name="Fréquence de fertilisation (jours)")
+    repotting_frequency = models.IntegerField(
+        default=365, verbose_name="Fréquence de rempotage (jours)")
+    pruning_frequency = models.IntegerField(
+        default=90, verbose_name="Fréquence de taille (jours)")
+
+    last_watering = models.DateField(
+        blank=True, null=True, verbose_name="Dernier arrosage")
+    last_fertilizing = models.DateField(
+        blank=True, null=True, verbose_name="Dernière fertilisation")
+    last_repotting = models.DateField(
+        blank=True, null=True, verbose_name="Dernier rempotage")
+    last_pruning = models.DateField(
+        blank=True, null=True, verbose_name="Dernière taille")
+
     class Meta:
         ordering = ['-created_at']
         verbose_name = "Plante"
@@ -39,3 +59,23 @@ class Plant(models.Model):
 
     def __str__(self):
         return self.name
+
+    def next_watering(self):
+        """ Retourne la date du prochain arrosage """
+        return self.last_watering + \
+            timedelta(days=self.watering_frequency) if self.last_watering else None
+
+    def next_fertilizing(self):
+        """ Retourne la date de la prochaine fertilisation """
+        return self.last_fertilizing + \
+            timedelta(days=self.fertilizing_frequency) if self.last_fertilizing else None
+
+    def next_repotting(self):
+        """ Retourne la date du prochain rempotage """
+        return self.last_repotting + \
+            timedelta(days=self.repotting_frequency) if self.last_repotting else None
+
+    def next_pruning(self):
+        """ Retourne la date de la prochaine taille """
+        return self.last_pruning + \
+            timedelta(days=self.pruning_frequency) if self.last_pruning else None
