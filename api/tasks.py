13b9c1f3d3829739d0f4863b939934064ae34cdb
Antoine GREUZARD <antoin.greuzard@gmail.com>
2025-03-17 13:43:09 +0100

    feat: add migrations

--- api/tasks.py
+++ api/tasks.py
@@ -10,22 +10,22 @@ def send_maintenance_reminders():
     for plant in plants:
         reminders = []
         if plant.next_watering() and plant.next_watering() <= today:
-            reminders.append(f"- Arrosage")
+            reminders.append("- Arrosage")
 
         if plant.next_fertilizing() and plant.next_fertilizing() <= today:
-            reminders.append(f"- Fertilisation")
+            reminders.append("- Fertilisation")
 
         if plant.next_repotting() and plant.next_repotting() <= today:
-            reminders.append(f"- Rempotage")
+            reminders.append("- Rempotage")
 
         if plant.next_pruning() and plant.next_pruning() <= today:
-            reminders.append(f"- Taille")
+            reminders.append("- Taille")
 
         if reminders:
             send_mail(
                 subject=f"Rappel d'entretien pour {
                     plant.name}",
-                message=f"Il est temps de s'occuper de votre plante :\n" +
+                message="Il est temps de s'occuper de votre plante :\n" +
                 "\n".join(reminders),
                 from_email="no-reply@plantes.com",
                 recipient_list=["user@example.com"],
